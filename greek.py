bc71a0a538cbe6d899862d6024b28fc6847fcb06
abdulrauf623 <82411397+abdulrauf623@users.noreply.github.com>
2024-09-04 00:12:03 -0700

    Created new function here

--- greek.py
+++ greek.py
@@ -2,3 +2,10 @@ def countFromOneToTen(number):
   for i in range(1,number):
     print(i)
 countFromOneToTen(11)
+
+
+#creating another function
+
+def anotherFunction(number):
+  print(number)
+anotherFunction(23)
